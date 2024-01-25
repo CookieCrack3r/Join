a335be7eea6502693631b6e0a9d568356b84dac7
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-01-25 11:57:23 +0100

    startet with drag and drop

--- Board/board.js
+++ Board/board.js
@@ -1,61 +1,113 @@
 let todos = [{
+    'id': '01',
     'category': 'User Story',
     'title': 'Kochwelt Page & Recipe Recommender',
     'description': 'Build start page with recipe recommendation...',
     'subtasks': '1/2 Subtasks',
-    'status': 'In Progress'
+    'status': 'in-progress'
 },
 {
+    'id': '02',
     'category': 'Technical Task',
     'title': 'HTML Base Template Creation',
     'description': 'Create reusable HTML bas templates',
     'subtasks': '',
-    'status': 'Await feedback'
+    'status': 'to-do'
 },
 {
+    'id': '03',
     'category': 'Technical Task',
     'title': 'CSS Architecture Planning',
     'description': 'Build start page with recipe recommendation...',
     'subtasks': '2/2 Subtasks',
-    'status': 'Done'
+    'status': 'done'
 },
 {
+    'id': '04',
     'category': 'User Story',
     'title': 'Daily Kochwelt Recipe',
     'description': 'Implement daily recipe and portion calculator',
     'subtasks': '',
-    'status': 'Await feedback'
+    'status': 'await-feedback'
 }];
 
+let currentDraggedElement;
 
 function init() {
-    renderKanbanboard();
+    updateHTML();
 }
 
-function renderKanbanboard() {
-    let board = document.getElementById('in-progress');
-    board.innerHTML = '';
+function updateHTML() {
+    updateAwaitFeedback();
+    updateInProgress();
+    updateToDo();
+    updateDone();
+}
 
-    for (let i = 0; i < todos.length; i++) {
-        let todo = todos[i];
-        let category = todo.category;
-        let title = todo.title;
-        let description = todo.description;
-        let subtasks = todo.subtasks;
-        let status = todo.status;
+function updateToDo() {
+    let to_do = todos.filter(t => t['status'] == 'to-do');
 
-        board.innerHTML += /*html*/`
-        <td>
-                            <div class="card">
-                                <span id="category" class="label">${category}</span>
+    document.getElementById('todo').innerHTML = '';
+
+    for (let i = 0; i < to_do.length; i++) {
+        let todo = to_do[i];
+        document.getElementById('todo').innerHTML += generateKanbanHTML(todo);
+    }
+}
+
+function updateInProgress() {
+    let progress = todos.filter(t => t['status'] == 'in-progress');
+
+    document.getElementById('in-progress').innerHTML = '';
+
+    for (let i = 0; i < progress.length; i++) {
+        let todo = progress[i];
+        document.getElementById('in-progress').innerHTML += generateKanbanHTML(todo);
+    }
+}
+
+function updateAwaitFeedback() {
+    let await = todos.filter(t => t['status'] == 'await-feedback');
+
+    document.getElementById('await-feedback').innerHTML = '';
+
+    for (let i = 0; i < await.length; i++) {
+        let todo = await[i];
+        document.getElementById('await-feedback').innerHTML += generateKanbanHTML(todo);
+    }
+}
+
+function updateDone() {
+    let done = todos.filter(t => t['status'] == 'done');
+
+    document.getElementById('done').innerHTML = '';
+
+    for (let i = 0; i < done.length; i++) {
+        let todo = done[i];
+        document.getElementById('done').innerHTML += generateKanbanHTML(todo);
+    }
+}
+
+function generateKanbanHTML(todo) {
+    let categoryColor = '';
+
+    if (todo['category'] === 'Technical Task') {
+        categoryColor = '#005bf8';
+    } else if (todo['category'] === 'User Story') {
+        categoryColor = '#FF7A00';
+    }
+
+    return `
+             <div draggable="true" ondragstart="startDraggin(${todo['id']})" class="card">
+             <span class="label" style="background-color: ${categoryColor};">${todo['category']}</span>
                                 <span class="description">
-                                    <h3>${title}</h3><br>${description}
+                                    <h3>${todo['title']}</h3><br>${todo['description']}
                                 </span>
                                 <div class="progress-section">
                                     <div class="progress-bar">
                                         <div class="progress"></div>
                                     </div>
-                                    <div>${subtasks}</div>
+                                    <div>${todo['subtasks']}</div>
                                 </div>
                                 <div class="members-and-priority">
                                     <div class="members">
@@ -68,9 +120,28 @@ function renderKanbanboard() {
                                     </div>
                                 </div>
                             </div>
-                        </td>
-        `;
-    }
+    `;
+}
+
+function startDraggin(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(status) {
+    todos[currentDraggedElement]['status'] = status;
+    updateHTML();
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add('dragsection-highlight');
+}
+
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove('dragsection-highlight');
 }
 
 function addTask() {
