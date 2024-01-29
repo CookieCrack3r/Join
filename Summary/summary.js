5844ca62dda3267a38547e092c3d12f65e479ffc
cookie <mail@vks.info>
2024-01-29 13:53:24 +0100

    added container

---
+++ Summary/summary.js
@@ -0,0 +1,20 @@
+let todos = [];
+let task = document.getElementById('task-todo');
+
+
+
+async function loadTodos(){
+    try {
+        todos = JSON.parse(await getItem('todos'));
+       
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+let progress = todos.filter(t => t['status'] == 'in-progress');
+task.innerHTML = progress.toString();
+
+   
+   
+
