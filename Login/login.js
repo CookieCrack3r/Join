a4cfef4798013244af35ab1b95a3dc49e3bbcc11
Hamzaba92 <hamzabaj1992@gmail.com>
2024-01-30 08:59:35 +0100

    Login&&GuestLogin function

--- Login/login.js
+++ Login/login.js
@@ -1,3 +1,6 @@
+
+
+
 //to reset the Z-Index of the Logo-animation in the first
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -15,5 +18,29 @@ document.addEventListener('DOMContentLoaded', function() {
 function redirectToSignUp(){
     window.location.pathname = '/SignUP/signup.html';
 }
+
+//this function is to login as a guest
+
+function loginAsGuest(){
+    window.location.pathname = '/Summary/summary.html';
+}
+
+// this function is to login with your registered Email and Password
+
+async function LoginRegistered(){
+    const email = document.getElementById('login_email_input');
+    const password = document.getElementById('login_password_input');
+
+    try{
+        users = JSON.parse(await getItem('users'));
+        user = users.find(u => u.email === email.value && u.password === password.value);
+    if(user){
+        await setItem('userName', email.password)
+        window.location.pathname = '/Summary/summary.html';}
+    }catch(e){console.error('Loading erorr', e);
+}
+    
+}
+
 
 
