57600dc7719f6482a1532344d1a9ed0bb8dbfea1
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-02-08 20:11:04 +0100

    contacts site designed

--- Contacts/contacts.js
+++ Contacts/contacts.js
@@ -1,5 +1,6 @@
 function addContact() {
     document.getElementById('addNewContact').classList.remove('displayNone');
+    document.getElementById('bg').style.display = 'flex';
 }
 
 function openContact() {
@@ -8,4 +9,11 @@ function openContact() {
 
 function closeCard() {
     document.getElementById('addNewContact').classList.add('displayNone');
+    document.getElementById('editContact').classList.add('displayNone');
+    document.getElementById('bg').style.display = 'none';
+}
+
+function editContact() {
+    document.getElementById('editContact').classList.remove('displayNone');
+    document.getElementById('bg').style.display = 'flex';
 }
