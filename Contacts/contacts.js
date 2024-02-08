4d11f086e57fe561542e09237b4c221fe438e9a9
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-02-08 19:43:20 +0100

    update Contacts

--- Contacts/contacts.js
+++ Contacts/contacts.js
@@ -1,3 +1,11 @@
+function addContact() {
+    document.getElementById('addNewContact').classList.remove('displayNone');
+}
+
 function openContact() {
-    
+    document.getElementById('contactContent').classList.remove('displayNone');
+}
+
+function closeCard() {
+    document.getElementById('addNewContact').classList.add('displayNone');
 }
