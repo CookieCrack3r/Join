a23f25c7f97788c843b28c1417086157b4680bfc
Hamzaba92 <hamzabaj1992@gmail.com>
2024-02-09 20:33:22 +0100

    render initials in all templates

--- Board/board.js
+++ Board/board.js
@@ -338,3 +338,34 @@ function removeHighlight(id) {
 function addTask() {
     window.location.href = '/AddTask/addTask.html';
 }
+
+
+//this function is to get the user initials
+async function getInitials(){
+    UserInitials = await getItem('userInitial');
+    UserName = await getItem('userName');
+    const kanban = document.getElementById("kanban");
+    kanban.innerHTML += `<div onclick="displayOptions()" id="initials">
+      ${UserInitials}
+      </div>`;
+}
+
+//this function is to open the submenu for the logout
+
+function displayOptions() {
+  const options = document.getElementById("options");
+  const isDisplayed = options.classList.toggle("dNone");
+ 
+  if (isDisplayed) {
+    document.getElementById('d_none_svg').style.display = 'none';
+  }
+
+  if (isDisplayed && !options.innerHTML.trim()) {
+    options.innerHTML = /*html*/`
+      <div class="option"><a href="/PrivacyPolicy/privacypolicy.html">Privacy Policy</a></div>
+      <div class="option"><a href="/LegalNotice/legalnotice.html">Legal Notice</a></div>
+      <div class="option" onclick="goToLogin()">Log out</div>
+    
+    `;
+  }
+}
--- Contacts/contacts.js
+++ Contacts/contacts.js
@@ -1,3 +1,8 @@
+async function init(){
+    getInitials();
+    displayOptions();
+}
+
 function addContact() {
     document.getElementById('addNewContact').classList.remove('displayNone');
     document.getElementById('bg').style.display = 'flex';
@@ -16,4 +21,34 @@ function closeCard() {
 function editContact() {
     document.getElementById('editContact').classList.remove('displayNone');
     document.getElementById('bg').style.display = 'flex';
+}
+
+//this function is to get the user initials
+async function getInitials(){
+    UserInitials = await getItem('userInitial');
+    UserName = await getItem('userName');
+    const kanban = document.getElementById("kanban");
+    kanban.innerHTML += `<div onclick="displayOptions()" id="initials">
+      ${UserInitials}
+      </div>`;
+}
+
+//this function is to open the submenu for the logout
+
+async function displayOptions() {
+  const options = document.getElementById("options");
+  const isDisplayed = options.classList.toggle("dNone");
+ 
+  if (isDisplayed) {
+    document.getElementById('d_none_svg').style.display = 'none';
+  }
+
+  if (isDisplayed && !options.innerHTML.trim()) {
+    options.innerHTML = /*html*/`
+      <div class="option"><a href="/PrivacyPolicy/privacypolicy.html">Privacy Policy</a></div>
+      <div class="option"><a href="/LegalNotice/legalnotice.html">Legal Notice</a></div>
+      <div class="option" onclick="goToLogin()">Log out</div>
+    
+    `;
+  }
 }
