1c2608b007a67d10a9f6bda7912bed2ccd371686
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-02-13 10:56:29 +0100

    added delete function

--- Contacts/contacts.js
+++ Contacts/contacts.js
@@ -7,13 +7,13 @@ let contacts = [];
 // }
 
 async function createContact() {
-    contacts_length = JSON.parse(await getItem('contacts')) || [];
-    let newcontactsId = contacts_length.length - 1;
-    newcontactsId++;
+  contacts_length = JSON.parse(await getItem('contacts')) || [];
+  let newcontactsId = contacts_length.length - 1;
+  newcontactsId++;
 
 
-    contacts = JSON.parse(await getItem('contacts')) || [];
-    contacts.push({
+  contacts = JSON.parse(await getItem('contacts')) || [];
+  contacts.push({
     id: newcontactsId,
     name: nameInput.value,
     mail: mail.value,
@@ -29,7 +29,7 @@ async function loadContacts() {
   try {
     contacts = JSON.parse(await getItem('contacts')) || [];
     generateContacts();
-    
+
   } catch (e) {
     console.error('Loading error:', e);
   }
@@ -45,38 +45,50 @@ async function generateContacts() {
 
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
+    const initials = getContactInitials(contact.name);
+    
     document.getElementById('allContacts').innerHTML += `
     <div>
-      <span class="alphabet">A<div class="borderBottom"></div></span>
+      <span class="alphabet">${initials[0]}<div class="borderBottom"></div></span>
       <div id="${contact.id}" class="contact" onclick="openContact(${i})">
-          <div class="contactSign">${contact.name.substring(0, 2)}</div><span>${contact.name}</span>
+          <div class="contactSign">${initials}</div><span>${contact.name}</span>
       </div>
     </div>
     `;
   }
- }
+}
 
 function openContact(i) {
+  const contact = contacts[i];
+  const initials = getContactInitials(contact.name);
+
   document.getElementById('contactContent').classList.remove('displayNone');
   document.getElementById('contactContent').innerHTML = `
   <div class="contactId">
-  <div class="bigContactSign">AM</div>
+  <div class="bigContactSign">${initials}</div>
   <div class="contactName">
-      <span>${contacts[i].name}</span>
+      <span>${contact.name}</span>
       <span>
           <button onclick="editContact()"><img src="img/edit.svg">Edit</button>
-          <button><img src="img/delete.svg">Delete</button>
+          <button onclick="deleteContact(${i})"><img src="img/delete.svg">Delete</button>
       </span>
   </div>
 </div>
 <div class="contactInfo">
   <h3>Contact Information</h3>
-  <span><b>Email</b><br><br><a href="mailto:${contacts[i].mail}">${contacts[i].mail}</a></span>
+  <span><b>Email</b><br><br><a href="mailto:${contact.mail}">${contact.mail}</a></span>
   <br>
   <br>
-  <span><b>Phone</b><br><br>${contacts[i].phone}</span>
+  <span><b>Phone</b><br><br>${contact.phone}</span>
 </div>
   `;
+}
+
+function getContactInitials(name) {
+  const nameParts = name.split(' ');
+  const firstNameInitial = nameParts[0][0];
+  const lastNameInitial = nameParts.length > 1 ? nameParts[nameParts.length - 1][0] : '';
+  return firstNameInitial + lastNameInitial;
 }
 
 function closeCard() {
@@ -89,6 +101,21 @@ function editContact() {
   document.getElementById('editContact').classList.remove('displayNone');
   document.getElementById('bg').style.display = 'flex';
 }
+
+async function deleteContact(i) {
+  try {
+    contacts = JSON.parse(await getItem('contacts')) || [];
+    contacts.splice(i, 1);
+    await setItem('contacts', JSON.stringify(contacts));
+    closeCard();
+    generateContacts();
+  } catch (e) {
+    console.error('Error deleting contact:', e);
+  }
+
+  document.getElementById('contactContent').classList.add('displayNone');
+}
+
 
 //this function is to get the user initials
 async function getInitials() {
