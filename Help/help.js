b728183ae9d497ba5e5276a403de00f079d4410e
Hamzaba92 <hamzabaj1992@gmail.com>
2024-02-21 11:06:33 +0100

    <a href> for Join Logo
    i give a href to the main logo of join in the header section, to redirect after click the logo to the summary site

---
+++ Help/help.js
@@ -0,0 +1,37 @@
+
+async function init(){
+    await getInitials();
+    displayOptions();
+}
+
+
+async function displayOptions() {
+    const options = document.getElementById("options");
+    const isDisplayed = options.classList.toggle("dNone");
+   
+    if (isDisplayed) {
+      document.getElementById('d_none_svg').style.display = 'none';
+    }
+  
+    if (isDisplayed && !options.innerHTML.trim()) {
+      options.innerHTML = /*html*/`
+        <div class="option"><a href="/PrivacyPolicy/privacypolicy.html">Privacy Policy</a></div>
+        <div class="option"><a href="/LegalNotice/legalnotice.html">Legal Notice</a></div>
+        <div class="option" onclick="goToLogin()">Log out</div>
+      
+      `;
+    }
+  }
+
+  async function getInitials(){
+    UserInitials = await getItem('userInitial');
+    UserName = await getItem('userName');
+    const kanban = document.getElementById("kanban");
+    kanban.innerHTML += `<div onclick="displayOptions()" id="initials">
+      ${UserInitials}
+      </div>`;
+}
+  
+    function goToLogin(){
+      window.location.pathname = '/Login/login.html';
+    }
