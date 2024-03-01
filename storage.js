f369517e36d443a5adc61058f3b91f99a077821b
Hamzaba92 <hamzabaj1992@gmail.com>
2024-03-01 20:13:07 +0100

    adding jsdoc comments

--- storage.js
+++ storage.js
@@ -3,20 +3,23 @@ const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
 //this function is to store the users in the server
 
-async function setItem(key, value){
-    const payload = {key, value, token:STORAGE_TOKEN};
-    return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)})
-    .then(res => res.json());
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
+        .then(res => res.json());
 }
 
-//this function is to get the user from the server with the key
-
-async function getItem(key){
+/**
+* this function is to get the user from the server with the key
+* @param STORAGE_URL - the url of the backendserver
+* @param STORAGE_TOKEN - the token who communicate with the backendserver
+* @param key - to get the data from the backendserver is only the key necessary
+**/
+async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => {
-        if(res.data){
+        if (res.data) {
             return res.data.value;
         } throw `Could not found data with "${key}".`;
-        
-        });
+    });
 }
