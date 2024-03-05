2e3adf78879dba79cce7e9751cc759a2b6fe4f76
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-03-05 13:32:47 +0100

    template js doc

--- Templates/template.js
+++ Templates/template.js
@@ -1,3 +1,7 @@
+/**
+ * Includes HTML content from external files into elements with the specified attribute.
+ * Uses the 'w3-include-html' attribute to determine the file path for inclusion.
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
